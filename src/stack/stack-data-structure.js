de8d77b5f31eeca8ec9f5dee9355dc3c3ad1dfeb
Gus <august.colonna@gmail.com>
2023-11-05 13:14:51 +0100

    solved all iterations

--- src/stack/stack-data-structure.js
+++ src/stack/stack-data-structure.js
@@ -5,25 +5,42 @@ class Stack {
   }
 
   canPush() {
-    // ... your code goes here
+    if (this.stackControl.length >= this.MAX_SIZE) {
+      return false;
+    } else if (this.stackControl.length < this.MAX_SIZE) {
+      return true;
+    }
   }
 
   isEmpty() {
-    // ... your code goes here
+    if (this.stackControl.length == 0) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   push(item) {
-    // ... your code goes here
+    if (this.canPush()) {
+      this.stackControl.push(item);
+    } else {
+      throw new Error("STACK_OVERFLOW");
+    }
+    return this.stackControl;
   }
 
   pop() {
-    // ... your code goes here
+    if (!this.isEmpty()) {
+      return this.stackControl.pop();
+    } else {
+      throw new Error("STACK_UNDERFLOW");
+    }
   }
 
   display() {
-    // ... your code goes here
-  }  
+    return this.stackControl;
+  }
 }
 
 // This is required to enable the automated tests, please ignore it.
-if (typeof module !== 'undefined') module.exports = Stack;
+if (typeof module !== "undefined") module.exports = Stack;
